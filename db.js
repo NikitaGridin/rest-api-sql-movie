adddffbd1d77e03a6592ef0f0f47183fbc23a677
Nikita <aruisialalin@yandex.ru>
2023-02-23 23:15:53 +0300

    chanhe password

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("@sequelize/core");
 
-module.exports = new Sequelize("aruisidp_mysql", "aruisidp_mysql", "mD*635Kk", {
+module.exports = new Sequelize("aruisidp_mysql", "aruisidp_mysql", "5xUQ11Y*", {
   host: "aruisidp.beget.tech",
   dialect: "mysql",
 });
